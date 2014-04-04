070f317e97d253409db47a136f261cd237e04de5
Nicholas Dujay <nickdujay@gmail.com>
2014-04-04 15:55:06 -0700

    Fixed test cases to stop spitting out errors

--- matplotlib/lib/mpl_toolkits/tests/test.sh
+++ matplotlib/lib/mpl_toolkits/tests/test.sh
@@ -1,4 +1,4 @@
 cd ../../../
 sudo rm -rf cover
-python tests.py --with-coverage --cover-html --cover-branch --cover-package=mpl_toolkits.mplot3d.axes3d mpl_toolkits.tests.test_errorbar3 mpl_toolkits.tests.test_errorbar2 #mpl_toolkits.tests.test_errorbar1
+python tests.py --with-coverage --cover-html --cover-branch --cover-package=mpl_toolkits.mplot3d.axes3d mpl_toolkits.tests.test_errorbar3 mpl_toolkits.tests.test_errorbar2 mpl_toolkits.tests.test_errorbar1
 
